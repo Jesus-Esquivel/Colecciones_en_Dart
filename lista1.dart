6bc9a44305121cfa5797ebb561fdb3de13383b46
Jesus-Esquivel <a.22308051281176@cbtis128.edu.mx>
2025-02-14 00:50:17 +0000

    Ejemplo de lista

---
+++ lista1.dart
@@ -0,0 +1,13 @@
+void main(){
+    print("Jesus Adrian Esquivel Mat 22308051281176 gpo G J");
+List<int> numeros = [10 , 20 ,30];
+print("Lista de numeros enteros");
+print(numeros);
+print("Primer elemento: ${numeros[0]}");
+print("segundo elemento: ${numeros[1]}");
+print("Tercer elemento: ${numeros[2]}");
+print("Elementos de la lista usando ciclo for: ");
+for(int i=0;i<numeros.length;i++){
+    print("Elementos en la posicion $i: ${numeros[i]}");
+}
+}
